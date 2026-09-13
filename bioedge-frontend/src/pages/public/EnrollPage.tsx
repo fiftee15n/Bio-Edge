6c5d76dad0f8e4965faa0a88c34efefd60df0368
fiftee15n <tamaljalam15@gmail.com>
2026-09-13 06:13:30 +0600

    Reposition Google auth button below form submit button on login, register, and modal flows

--- bioedge-frontend/src/pages/public/EnrollPage.tsx
+++ bioedge-frontend/src/pages/public/EnrollPage.tsx
@@ -519,25 +519,7 @@ export const EnrollPage: React.FC = () => {
                           </div>
                         )}
 
-                        {/* Continue with Google */}
-                        <button
-                          type="button"
-                          onClick={handleGoogleAuth}
-                          disabled={authLoading}
-                          className="btn-google-auth-full"
-                        >
-                          <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M17.64 9.2c0-.637-.057-1.251-.164-1.84H9v3.481h4.844c-.209 1.125-.843 2.078-1.796 2.717v2.258h2.908c1.702-1.567 2.684-3.874 2.684-6.616z" fill="#4285F4"/>
-                            <path d="M9 18c2.43 0 4.467-.806 5.956-2.184l-2.908-2.258c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 0 0 9 18z" fill="#34A853"/>
-                            <path d="M3.964 10.707c-.18-.54-.282-1.117-.282-1.707s.102-1.167.282-1.707V4.961H.957A8.996 8.996 0 0 0 0 9c0 1.452.348 2.827.957 4.039l3.007-2.332z" fill="#FBBC05"/>
-                            <path d="M9 3.58c1.321 0 2.508.454 3.44 1.345l2.582-2.58C13.463.891 11.426 0 9 0A8.997 8.997 0 0 0 .957 4.961L3.964 7.293C4.672 5.166 6.656 3.58 9 3.58z" fill="#EA4335"/>
-                          </svg>
-                          <span>Continue with Google</span>
-                        </button>
 
-                        <div className="auth-divider">
-                          <span>Or Fill Out Student Details</span>
-                        </div>
 
                         {/* Registration Form */}
                         {authMode === 'register' && (
@@ -690,6 +672,26 @@ export const EnrollPage: React.FC = () => {
                             </button>
                           </form>
                         )}
+
+                        {/* Continue with Google (After Form) */}
+                        <div className="auth-divider">
+                          <span>Or continue with</span>
+                        </div>
+
+                        <button
+                          type="button"
+                          onClick={handleGoogleAuth}
+                          disabled={authLoading}
+                          className="btn-google-auth-full"
+                        >
+                          <svg width="18" height="18" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M17.64 9.2c0-.637-.057-1.251-.164-1.84H9v3.481h4.844c-.209 1.125-.843 2.078-1.796 2.717v2.258h2.908c1.702-1.567 2.684-3.874 2.684-6.616z" fill="#4285F4"/>
+                            <path d="M9 18c2.43 0 4.467-.806 5.956-2.184l-2.908-2.258c-.806.54-1.837.86-3.048.86-2.344 0-4.328-1.584-5.036-3.711H.957v2.332A8.997 8.997 0 0 0 9 18z" fill="#34A853"/>
+                            <path d="M3.964 10.707c-.18-.54-.282-1.117-.282-1.707s.102-1.167.282-1.707V4.961H.957A8.996 8.996 0 0 0 0 9c0 1.452.348 2.827.957 4.039l3.007-2.332z" fill="#FBBC05"/>
+                            <path d="M9 3.58c1.321 0 2.508.454 3.44 1.345l2.582-2.58C13.463.891 11.426 0 9 0A8.997 8.997 0 0 0 .957 4.961L3.964 7.293C4.672 5.166 6.656 3.58 9 3.58z" fill="#EA4335"/>
+                          </svg>
+                          <span>Continue with Google</span>
+                        </button>
                       </div>
                     ) : (
                       /* OTP Verification in place */
